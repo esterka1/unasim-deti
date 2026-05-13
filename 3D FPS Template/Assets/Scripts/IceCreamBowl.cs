04a7bf9cfbcc49b1130bf64318435c3e48cf9fe4
esterka1 <4mandic.ester@skolamichael.cz>
2026-05-13 16:27:32 +0200

    neco

--- 3D FPS Template/Assets/Scripts/IceCreamBowl.cs
+++ 3D FPS Template/Assets/Scripts/IceCreamBowl.cs
@@ -14,31 +14,44 @@ public class IceCreamBowl : MonoBehaviour
     {
         IceCreamData.addedIngredients.Clear();
         addedIngredients.Clear();
+
+        Debug.Log("IceCreamBowl ready.");
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Něco se dotklo misky: " + other.name);
+
         if (!other.CompareTag("food"))
+        {
+            Debug.Log("Objekt nemá tag food: " + other.name);
             return;
+        }
 
         Ingredient ingredient = other.GetComponent<Ingredient>();
 
         if (ingredient == null)
+        {
+            Debug.Log("Objekt nemá Ingredient script: " + other.name);
             return;
+        }
 
         if (addedIngredients.Count >= maxIngredients)
+        {
+            Debug.Log("Miska už má maximum ingrediencí.");
             return;
+        }
 
         addedIngredients.Add(ingredient.ingredientName);
-
         IceCreamData.addedIngredients = new List<string>(addedIngredients);
+
+        Debug.Log("PŘIDÁNO DO ZMRZLINY: " + ingredient.ingredientName);
+        Debug.Log("Počet ingrediencí: " + addedIngredients.Count);
 
         if (addIngredientEffect != null)
         {
             addIngredientEffect.Play();
         }
-
-        Debug.Log("Přidaná ingredience do zmrzliny: " + ingredient.ingredientName);
 
         Destroy(other.gameObject);
     }
